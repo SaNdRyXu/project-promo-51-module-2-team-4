7d2272557611b8563903a259bb5c2f2a1d52af0a
Yolanda Tuñón <yolandatuin@gmail.com>
2025-05-31 10:34:01 +0200

    Añadidos enlaces a JSs

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
 
+import 'functions/1_home-button.js';
+import 'functions/2_predesigned-or-submit-image.js';
 import 'functions/3_select-theme.js';
-
+import 'functions/4_input-title.js';
+import 'functions/5_input-subtitle.js';
+import 'functions/6_select-platform.js';
+import 'functions/7_input-age.js';
+import 'functions/8_select-company-logo.js';
+import 'functions/9_save-button.js';
+import 'functions/10_share-section.js';
 
 
 
