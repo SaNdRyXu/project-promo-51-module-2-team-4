6842ce6e0c5a38db9ef35b0bcd2a4af31a90636c
Sandra <sandra_13_c70@hotmail.com>
2025-06-03 22:20:48 +0200

    platforms listas

--- src/js/functions/select-platform.js
+++ src/js/functions/select-platform.js
@@ -6,19 +6,30 @@ const previewimagePlatform = document.querySelector('.js-imgrightbutton');
 
 function updatePlatform() {
     const selectedPlatform = document.querySelector('input[name="platform"]:checked').value;
+    const previewTop = document.getElementById('previewTop');
+    const previewimagePlatform = document.getElementById('previewimagePlatform');
+
+    previewTop.className = "preview-top"; 
+    previewimagePlatform.src = "images/ps5.png";
+    previewimagePlatform.alt = "Imagen de PS5";
+    
 
     if (selectedPlatform === "PS5") {
         previewimagePlatform.src = "images/ps5.png";
-        previewimagePlatform.alt = "Imagen de la plataforma PS5";
+        previewimagePlatform.alt = "Imagen de PS5";
+
     } else if (selectedPlatform === "PC") {
         previewimagePlatform.src = "images/pc.png";
-        previewimagePlatform.alt = "Imagen de la plataforma PC";
+        previewimagePlatform.alt = "Imagen de PC";
+        previewTop.className ="preview-pc";
     } else if (selectedPlatform === "Xbox") {
-        previewimagePlatform.src = "images/xbox.png";
-        previewimagePlatform.alt = "Imagen de la plataforma Xbox";
+        previewimagePlatform.src = "images/xbox.svg";
+        previewimagePlatform.alt = "Imagen de Xbox";
+        previewTop.className ="preview-xbox";
     } else if (selectedPlatform === "Switch") {
-        previewimagePlatform.src = "images/switch.png";
-        previewimagePlatform.alt = "Imagen de la plataforma Nintendo Switch";
+        previewimagePlatform.src = "images/switch.svg";
+        previewimagePlatform.alt = "Imagen de Nintendo Switch";
+        previewTop.className ="preview-switch";
     }
 }
 
